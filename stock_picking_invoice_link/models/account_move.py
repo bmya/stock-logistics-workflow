847a35018b5976ddd38c46f71bd36b50d2908399
haular <aular.hector.dev@gmail.com>
2025-04-29 10:19:25 -0400

    [FIX] Se comenta el campo picking_ids debido a su alto costo computacional.
    Es necesario respaldar la tabla account_move_stock_picking_rel, ya que todos
    sus datos se perderán durante el proceso. Posteriormente, se debe pasar nuevamente
    la información de la tabla que contiene el respaldo a account_move_stock_picking_rel.

--- stock_picking_invoice_link/models/account_move.py
+++ stock_picking_invoice_link/models/account_move.py
@@ -15,7 +15,8 @@ class AccountMove(models.Model):
         comodel_name="stock.picking",
         string="Related Pickings",
         store=True,
-        compute="_compute_picking_ids",
+        # TODO remove for avoid recompute upgrade
+        # compute="_compute_picking_ids",
         help="Related pickings (only when the invoice has been generated from a sale "
         "order).",
     )
